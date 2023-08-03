7f68cb363523da9b5cb3d4807113d5e97da3f23a
zhangziyu <hfzl@1234>
2023-08-03 14:53:31 +0800

    消息提示

---
+++ src/components/Notice/NoticeList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NoticeItem from "./noticeItem.vue";
+
+const props = defineProps({
+  list: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
+
+<template>
+  <div v-if="props.list.length">
+    <NoticeItem
+        v-for="(item, index) in props.list"
+        :noticeItem="item"
+        :key="index"
+    />
+  </div>
+  <el-empty v-else description="暂无数据" />
+</template>
--- src/views/Layout/components/CommonHeader/components/HeaderTop.vue
+++ src/views/Layout/components/CommonHeader/components/HeaderTop.vue
@@ -5,6 +5,7 @@ import {ElMessage} from "element-plus";
 import {useUserStore} from "@/store/moudles/user.js";
 import {useColorStore} from "@/store/moudles/settings.js";
 import DarkSwitch from '@/components/DarkSwitch/index.vue'
+import NoticeList from "@/components/Notice/NoticeList.vue";
 
 const router = useRouter()
 const userStore = useUserStore()
@@ -31,6 +32,137 @@ const onLogout = () => {
     customClass: 'pure-message'
   })
 }
+const noticesData = ref([
+  {
+    key: "1",
+    name: "通知",
+    list: [
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png",
+        title: "你收到了 12 份新周报",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png",
+        title: "你推荐的 前端高手 已通过第三轮面试",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png",
+        title: "这种模板可以区分多种通知类型",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png",
+        title:
+            "展示标题内容超过一行后的处理方式，如果内容超过1行将自动截断并支持tooltip显示完整标题。",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png",
+        title: "左侧图标用于区分不同的类型",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png",
+        title: "左侧图标用于区分不同的类型",
+        datetime: "一年前",
+        description: "",
+        type: "1"
+      }
+    ]
+  },
+  {
+    key: "2",
+    name: "消息",
+    list: [
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
+        title: "李白 评论了你",
+        description: "长风破浪会有时,直挂云帆济沧海",
+        datetime: "一年前",
+        type: "2"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
+        title: "李白 回复了你",
+        description: "行路难，行路难，多歧路，今安在。",
+        datetime: "一年前",
+        type: "2"
+      },
+      {
+        avatar:
+            "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
+        title: "标题",
+        description:
+            "请将鼠标移动到此处，以便测试超长的消息在此处将如何处理。本例中设置的描述最大行数为2，超过2行的描述内容将被省略并且可以通过tooltip查看完整内容",
+        datetime: "一年前",
+        type: "2"
+      }
+    ]
+  },
+  {
+    key: "3",
+    name: "代办",
+    list: [
+      {
+        avatar: "",
+        title: "任务名称",
+        description: "任务需要在 2022-11-16 20:00 前启动",
+        datetime: "",
+        extra: "未开始",
+        status: "info",
+        type: "3"
+      },
+      {
+        avatar: "",
+        title: "第三方紧急代码变更",
+        description:
+            "一拳提交于 2022-11-16，需在 2022-11-18 前完成代码变更任务",
+        datetime: "",
+        extra: "马上到期",
+        status: "danger",
+        type: "3"
+      },
+      {
+        avatar: "",
+        title: "信息安全考试",
+        description: "指派小仙于 2022-12-12 前完成更新并发布",
+        datetime: "",
+        extra: "已耗时 8 天",
+        status: "warning",
+        type: "3"
+      },
+      {
+        avatar: "",
+        title: "vue-pure-admin 版本发布",
+        description: "vue-pure-admin 版本发布",
+        datetime: "",
+        extra: "进行中",
+        type: "3"
+      }
+    ]
+  }
+])
+const activeKey = ref(noticesData.value[0].key)
 onMounted(() => colorStore.setColor())
 </script>
 
@@ -53,11 +185,34 @@ onMounted(() => colorStore.setColor())
         <Search/>
       </el-icon>
 
-      <el-icon class="bell">
-        <el-badge :value="100" :max="10" class="item">
-          <Bell/>
-        </el-badge>
-      </el-icon>
+      <el-dropdown trigger="click" placement="bottom-end">
+        <span class="dropdown-badge navbar-bg-hover select-none">
+          <el-icon class="bell">
+            <el-badge :value="100" :max="99" class="item">
+              <Bell/>
+            </el-badge>
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-tabs :stretch="true" v-model="activeKey" class="dropdown-tabs">
+              <template v-for="item in noticesData" :key="item.key">
+                <el-tab-pane
+                    :label="`${item.name}(${item.list.length})`"
+                    :name="`${item.key}`"
+                >
+                  <el-scrollbar max-height="330px">
+                    <div class="noticeList-container">
+                      <NoticeList :list="item.list" />
+                    </div>
+                  </el-scrollbar>
+                </el-tab-pane>
+              </template>
+            </el-tabs>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+
 
       <div class="user">
         <el-dropdown ref="dropdown" trigger="contextmenu">
@@ -135,7 +290,6 @@ onMounted(() => colorStore.setColor())
   .menu-item {
     display: flex;
     justify-content: flex-end;
-
     .search {
       width: 40px;
       height: 48px;
@@ -145,7 +299,6 @@ onMounted(() => colorStore.setColor())
         background: #f6f6f6
       }
     }
-
     .bell {
       width: 50px;
       height: 48px;
@@ -155,7 +308,6 @@ onMounted(() => colorStore.setColor())
         background: #f6f6f6
       }
     }
-
     .user {
       height: 48px;
       display: flex;
@@ -166,6 +318,10 @@ onMounted(() => colorStore.setColor())
       &:hover {
         background: #f6f6f6
       }
+    }
+    svg {
+      width: 16px;
+      height: 16px;
     }
   }
 }
@@ -241,5 +397,42 @@ onMounted(() => colorStore.setColor())
     border-radius: 4px;
     cursor: pointer;
   }
+}
+.dropdown-badge {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 48px;
+  width: 60px;
+  cursor: pointer;
+
+  .header-notice-icon {
+    font-size: 18px;
+  }
+}
+.dropdown-tabs {
+  width: 330px;
+
+  .noticeList-container {
+    padding: 15px 24px 0 24px;
+  }
+
+  :deep(.el-tabs__header) {
+    margin: 0;
+  }
+
+  :deep(.el-tabs__nav-wrap)::after {
+    height: 1px;
+  }
+  //
+  //// 如果上面的 notices 长度大于 3 请注释掉下面代码
+  //:deep(.el-tabs__nav-wrap) {
+  //  padding: 0 36px 0 36px;
+  //}
+  //
+  //// 如果上面的 notices 长度大于 3 请注释掉下面代码
+  //:deep(.el-tabs__active-bar) {
+  //  margin: 0 36px 0 36px;
+  //}
 }
 </style>
