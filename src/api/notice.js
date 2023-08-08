8628008dd9d979d1a2cf7031a4fb06426aec320f
zhangziyu <1772127579@qq.com>
2023-08-08 19:43:43 +0800

    删除某用户消息

--- src/api/notice.js
+++ src/api/notice.js
@@ -6,23 +6,32 @@ export const sendNoticeApi = (data) => request({
     method: 'post',
     data
 })
-
 // 获取当前用户的消息
 export const getNoticeApi = () => request({
     url: '/api/notice/',
     method: 'get'
 })
-
 // 获取所有消息
 export const getAllNoticeApi = (data) => request({
     url: '/api/all_notice',
     method: 'get',
     data
 })
-
 // 删除对应消息
 export const deleteNoticeApi = (id) => request({
     url: '/api/all_notice',
     method: 'delete',
     data: {id}
+})
+// 获取对应用户消息
+export const getUserNoticeApi = (id) => request({
+    url: '/api/user_notice',
+    method: 'get',
+    data: {id}
+})
+// 删除对应用户消息
+export const deleteUserNoticeApi = (data) => request({
+    url: '/api/user_notice/',
+    method: 'delete',
+    data
 })
--- src/views/Notice/index.vue
+++ src/views/Notice/index.vue
@@ -1,9 +1,10 @@
 <script setup>
 import {ref, onMounted} from 'vue'
-import {getAllNoticeApi, deleteNoticeApi} from "@/api/notice.js";
+import {getAllNoticeApi, deleteNoticeApi, getUserNoticeApi, deleteUserNoticeApi} from "@/api/notice.js";
 import {multiDeleteNoticeApi} from "@/api/multi.js";
 import moment from "moment";
 import {Delete, InfoFilled, Refresh, Search, View} from "@element-plus/icons-vue";
+import {ElMessage} from "element-plus";
 
 // 消息数据
 const noticeData = ref([])
@@ -76,6 +77,31 @@ const multiDelete = () => {
     getNoticeData()
   })
 }
+// 查看消息对应用户
+const noticeUserVisible = ref(false)
+const noticeUserData = ref([])
+const noticeId = ref(2)
+// 获取用户对应消息
+const getUserNoticeData = async (id) => {
+  noticeId.value = id
+  const res = await getUserNoticeApi(id)
+  noticeUserData.value = res.data.map(item => {
+    item.date_joined = moment(item.date_joined).format('YYYY-MM-DD')
+    return item
+  })
+  noticeUserVisible.value = true
+}
+// 删除用户消息
+const deleteUserNotice = async (id) => {
+ await deleteUserNoticeApi({noticeId: noticeId.value, userId: id})
+  ElMessage({
+    type: 'success',
+    message: '删除成功',
+    customClass: 'pure-message'
+  })
+  getUserNoticeData(noticeId.value)
+}
+
 onMounted(() => getNoticeData())
 </script>
 
@@ -106,7 +132,7 @@ onMounted(() => getNoticeData())
         <el-table-column fixed type="selection" width="55" />
         <el-table-column label="消息标识" width="120" align="center">
           <template #default="scope">
-            <img class="notice-avatar" :src="scope.row.avatar" alt="消息标识">
+            <img class="notice-avatar" :src="scope.row.avatar">
           </template>
         </el-table-column>
         <el-table-column prop="title" label="消息标题" width="180" align="center"/>
@@ -121,7 +147,7 @@ onMounted(() => getNoticeData())
                 content="查看收到消息的用户"
                 placement="top-end"
             >
-              <el-button type="primary" :icon="View" size="small" />
+              <el-button type="primary" :icon="View" size="small" @click="getUserNoticeData(scope.row.id)" />
             </el-tooltip>
             <el-popconfirm
                 confirm-button-text="确定"
@@ -152,6 +178,34 @@ onMounted(() => getNoticeData())
         @current-change="handleCurrentChange"
     />
   </el-card>
+
+  <el-dialog v-model="noticeUserVisible" title="Shipping address">
+    <el-table :data="noticeUserData">
+      <el-table-column prop="username" label="用户名" align="center"/>
+      <el-table-column prop="name" label="姓名" align="center"/>
+      <el-table-column prop="gender" label="性别" align="center"/>
+      <el-table-column prop="age" label="年龄" align="center"/>
+      <el-table-column prop="phone" label="联系方式" align="center"/>
+      <el-table-column prop="date_joined" label="注册时间" align="center"/>
+      <el-table-column prop="role" label="用户角色" align="center"/>
+      <el-table-column fixed="right" label="操作" align="center">
+        <template #default="scope">
+          <el-popconfirm
+              confirm-button-text="确定"
+              cancel-button-text="取消"
+              :icon="InfoFilled"
+              icon-color="#626AEF"
+              title="你确定删除吗?"
+              @confirm="deleteUserNotice(scope.row.id)"
+          >
+            <template #reference>
+              <el-button type="danger" :icon="Delete" size="small" />
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-dialog>
 </div>
 </template>
 
