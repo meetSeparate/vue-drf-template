49a821ec155d7064b9d537cc053fd90e8535541e
zhangziyu <1772127579@qq.com>
2023-08-10 17:30:31 +0800

    更换项目本地端口

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -7,33 +7,35 @@ import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 import path from 'path'
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        // ElementPlus按需导入
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        createSvgIconsPlugin({
-            // Specify the icon folder to be cached
-            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-            // Specify symbolId format
-            symbolId: 'icon-[dir]-[name]',
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+    return defineConfig({
+        plugins: [
+            vue(),
+            // ElementPlus按需导入
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+            createSvgIconsPlugin({
+                // Specify the icon folder to be cached
+                iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+                // Specify symbolId format
+                symbolId: 'icon-[dir]-[name]',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: process.env.VITE_PORT,
+            open: false,
         }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: '9000',
-        open: false,
-    }
-})
+    })
+}
